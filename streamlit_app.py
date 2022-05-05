3cbf6a02277606036ff9b71a1213fbac45f4a690
adamkowaltotwojstary <adamkowaltotwojstary@gmail.com>
2022-05-05 12:12:16 +0200

    API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
+import requests
 
+response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
@@ -14,4 +16,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruits_selected=streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
 fruits_to_show=my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(fruits_to_show,['Avocado','Strawberries'])
+streamlit.dataframe(fruits_to_show)
+
+streamlit.header('Fruityvice Fruit Advice')
+streamlit.text(response.json())
